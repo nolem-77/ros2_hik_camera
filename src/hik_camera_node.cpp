4146ebe1cb8a32fb9f049e97375174d4eb63839f
nolem <1258124282@qq.com>
2022-04-15 16:56:10 +0800

    Initial commit

---
+++ src/hik_camera_node.cpp
@@ -0,0 +1,196 @@
+#include "MvCameraControl.h"
+// ROS
+#include <camera_info_manager/camera_info_manager.hpp>
+#include <image_transport/image_transport.hpp>
+#include <rclcpp/logging.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/camera_info.hpp>
+#include <sensor_msgs/msg/image.hpp>
+
+namespace hik_camera {
+class HikCameraNode : public rclcpp::Node {
+public:
+  explicit HikCameraNode(const rclcpp::NodeOptions &options)
+      : Node("hik_camera", options) {
+    RCLCPP_INFO(this->get_logger(), "Starting HikCameraNode!");
+
+    MV_CC_DEVICE_INFO_LIST DeviceList;
+    // enum device
+    nRet = MV_CC_EnumDevices(MV_USB_DEVICE, &DeviceList);
+    RCLCPP_INFO(this->get_logger(), "Found camera count = %d",
+                DeviceList.nDeviceNum);
+
+    while (DeviceList.nDeviceNum == 0) {
+      RCLCPP_ERROR(this->get_logger(), "No camera found!");
+      RCLCPP_INFO(this->get_logger(), "Enum state: [%x]", nRet);
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+      nRet = MV_CC_EnumDevices(MV_USB_DEVICE, &DeviceList);
+    }
+    RCLCPP_INFO(this->get_logger(), "Find camera!");
+
+    MV_CC_CreateHandle(&camera_handle_, DeviceList.pDeviceInfo[0]);
+
+    MV_CC_OpenDevice(camera_handle_);
+
+    // get camera infomation
+    MV_CC_GetImageInfo(camera_handle_, &img_info_);
+    image_msg_.data.reserve(img_info_.nHeightMax * img_info_.nHeightMax * 3);
+
+    // Init convert param
+    ConvertParam_.nWidth = img_info_.nWidthMax;
+    ConvertParam_.nHeight = img_info_.nHeightMax;
+    ConvertParam_.enDstPixelType = PixelType_Gvsp_RGB8_Packed;
+
+    bool use_sensor_data_qos =
+        this->declare_parameter("use_sensor_data_qos", false);
+    auto qos = use_sensor_data_qos ? rmw_qos_profile_sensor_data
+                                   : rmw_qos_profile_default;
+    camera_pub_ =
+        image_transport::create_camera_publisher(this, "image_raw", qos);
+
+    declareParameters();
+
+    MV_CC_StartGrabbing(camera_handle_);
+
+    // Load camera info
+    camera_name_ = this->declare_parameter("camera_name", "narrow_stereo");
+    camera_info_manager_ =
+        std::make_unique<camera_info_manager::CameraInfoManager>(this,
+                                                                 camera_name_);
+    auto camera_info_url = this->declare_parameter(
+        "camera_info_url", "package://hik_camera/config/camera_info.yaml");
+    if (camera_info_manager_->validateURL(camera_info_url)) {
+      camera_info_manager_->loadCameraInfo(camera_info_url);
+      camera_info_msg_ = camera_info_manager_->getCameraInfo();
+    } else {
+      RCLCPP_WARN(this->get_logger(), "Invalid camera info URL: %s",
+                  camera_info_url.c_str());
+    }
+
+    params_callback_handle_ = this->add_on_set_parameters_callback(std::bind(
+        &HikCameraNode::parametersCallback, this, std::placeholders::_1));
+
+    capture_thread_ = std::thread{[this]() -> void {
+      MV_FRAME_OUT OutFrame;
+
+      RCLCPP_INFO(this->get_logger(), "Publishing image!");
+
+      image_msg_.header.frame_id = "camera_optical_frame";
+      image_msg_.encoding = "rgb8";
+
+      while (rclcpp::ok()) {
+        nRet = MV_CC_GetImageBuffer(camera_handle_, &OutFrame, 1000);
+        if (MV_OK == nRet) {
+          ConvertParam_.pDstBuffer = image_msg_.data.data();
+          ConvertParam_.nDstBufferSize = image_msg_.data.size();
+          ConvertParam_.pSrcData = OutFrame.pBufAddr;
+          ConvertParam_.nSrcDataLen = OutFrame.stFrameInfo.nFrameLen;
+          ConvertParam_.enSrcPixelType = OutFrame.stFrameInfo.enPixelType;
+
+          MV_CC_ConvertPixelType(camera_handle_, &ConvertParam_);
+
+          camera_info_msg_.header.stamp = image_msg_.header.stamp = this->now();
+          image_msg_.height = OutFrame.stFrameInfo.nHeight;
+          image_msg_.width = OutFrame.stFrameInfo.nWidth;
+          image_msg_.step = OutFrame.stFrameInfo.nWidth * 3;
+          image_msg_.data.resize(image_msg_.width * image_msg_.height * 3);
+          camera_pub_.publish(image_msg_, camera_info_msg_);
+
+          MV_CC_FreeImageBuffer(camera_handle_, &OutFrame);
+        } else {
+          RCLCPP_INFO(this->get_logger(), "Get buffer failed! nRet: [%x]",
+                      nRet);
+        }
+      }
+    }};
+  }
+
+  ~HikCameraNode() {
+    if (capture_thread_.joinable()) {
+      capture_thread_.join();
+    }
+    if (camera_handle_) {
+      MV_CC_StopGrabbing(camera_handle_);
+      MV_CC_CloseDevice(camera_handle_);
+      MV_CC_DestroyHandle(&camera_handle_);
+    }
+    RCLCPP_INFO(this->get_logger(), "HikCameraNode destroyed!");
+  }
+
+private:
+  void declareParameters() {
+    rcl_interfaces::msg::ParameterDescriptor param_desc;
+    MVCC_FLOATVALUE fValue;
+    param_desc.integer_range.resize(1);
+    param_desc.integer_range[0].step = 1;
+    // Exposure time
+    param_desc.description = "Exposure time in microseconds";
+    MV_CC_GetFloatValue(camera_handle_, "ExposureTime", &fValue);
+    param_desc.integer_range[0].from_value = fValue.fMin;
+    param_desc.integer_range[0].to_value = fValue.fMax;
+    double exposure_time =
+        this->declare_parameter("exposure_time", 5000, param_desc);
+    RCLCPP_INFO(this->get_logger(), "Exposure time: %f", exposure_time);
+
+    // Gain
+    param_desc.description = "Gain";
+    MV_CC_GetFloatValue(camera_handle_, "Gain", &fValue);
+    param_desc.integer_range[0].from_value = fValue.fMin;
+    param_desc.integer_range[0].to_value = fValue.fMax;
+    double gain = this->declare_parameter("gain", fValue.fCurValue, param_desc);
+    MV_CC_SetFloatValue(camera_handle_, "Gain", gain);
+    RCLCPP_INFO(this->get_logger(), "Gain: %f", gain);
+  }
+
+  rcl_interfaces::msg::SetParametersResult
+  parametersCallback(const std::vector<rclcpp::Parameter> &parameters) {
+    rcl_interfaces::msg::SetParametersResult result;
+    result.successful = true;
+    for (const auto &param : parameters) {
+      if (param.get_name() == "exposure_time") {
+        int status =
+            MV_CC_SetFloatValue(camera_handle_, "ExposureTime", param.as_int());
+        if (MV_OK != status) {
+          result.successful = false;
+          result.reason =
+              "Failed to set exposure time, status = " + std::to_string(status);
+        }
+      } else if (param.get_name() == "gain") {
+        int status =
+            MV_CC_SetFloatValue(camera_handle_, "Gain", param.as_double());
+        if (MV_OK != status) {
+          result.successful = false;
+          result.reason =
+              "Failed to set gain, status = " + std::to_string(status);
+        }
+      } else {
+        result.successful = false;
+        result.reason = "Unknown parameter: " + param.get_name();
+      }
+    }
+    return result;
+  }
+
+  sensor_msgs::msg::Image image_msg_;
+
+  image_transport::CameraPublisher camera_pub_;
+
+  int nRet = MV_OK;
+  void *camera_handle_;
+  MV_IMAGE_BASIC_INFO img_info_;
+
+  MV_CC_PIXEL_CONVERT_PARAM ConvertParam_;
+
+  std::string camera_name_;
+  std::unique_ptr<camera_info_manager::CameraInfoManager> camera_info_manager_;
+  sensor_msgs::msg::CameraInfo camera_info_msg_;
+
+  std::thread capture_thread_;
+
+  OnSetParametersCallbackHandle::SharedPtr params_callback_handle_;
+};
+} // namespace hik_camera
+
+#include "rclcpp_components/register_node_macro.hpp"
+
+RCLCPP_COMPONENTS_REGISTER_NODE(hik_camera::HikCameraNode)
