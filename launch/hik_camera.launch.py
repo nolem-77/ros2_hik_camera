c43dd493c12c78fc977d8459ee237f290aa9f852
ChenJun <chen.junn@outlook.com>
2022-07-03 16:25:52 +0800

    Set env in launch

--- launch/hik_camera.launch.py
+++ launch/hik_camera.launch.py
@@ -1,25 +1,31 @@
 import os
 
-from ament_index_python.packages import get_package_share_directory
+from ament_index_python.packages import get_package_share_directory, get_package_prefix
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
+from launch.substitutions import LaunchConfiguration, EnvironmentVariable
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    sdk_lib_path = get_package_prefix('hik_camera') + '/hikSDK' + '/lib'
+
     params_file = os.path.join(
         get_package_share_directory('hik_camera'), 'config', 'camera_params.yaml')
 
     camera_info_url = 'package://hik_camera/config/camera_info.yaml'
 
     return LaunchDescription([
+        SetEnvironmentVariable(name='LD_LIBRARY_PATH',
+                               value=[EnvironmentVariable('LD_LIBRARY_PATH'), ':', sdk_lib_path]),
+
         DeclareLaunchArgument(name='params_file',
                               default_value=params_file),
         DeclareLaunchArgument(name='camera_info_url',
                               default_value=camera_info_url),
         DeclareLaunchArgument(name='use_sensor_data_qos',
                               default_value='false'),
+
         Node(
             package='hik_camera',
             executable='hik_camera_node',
